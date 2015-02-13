6aa6825beb71ff222ef9e07b5a4bdee069fce22c
Ron <ronpark9@gmail.com>
2015-02-13 10:08:18 -0800

    Add functionality for 3 types of triangles and for when it is not.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,32 @@
 var triangles = function(side1, side2, side3) {
+	var side1 = parseInt(side1);
+	var side2 = parseInt(side2);
+	var side3 = parseInt(side3);
+
+	if (side1 === side2 && side2 === side3 && side3 === side1) {
+		return " an Equilateral";
+	} else if ((side1 === side2 || side2 === side3 || side3 === side1) &&
+						 !(side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side1 + side2)) {
+		return " an Isosceles";
+	} else if ((side1 !== side2 && side2 !== side3 && side3 !== side1) &&
+						 !(side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side1 + side2)) {
+		return " a Scalene";
+	} else {
+		return " NOT a";
+	}
 
 };
 
-// $(document).ready(function() {
-// 	$("form#leap-year").submit(function(event) {
-// 		var year = parseInt($("input#year").val());
-//
-// 		$(".year").text(year);
-//
-// 		if(!leapYear(year)) {
-// 			$(".not").text("not");
-// 		} else {
-// 			$(".not").text("");
-// 		}
-//
-// 		$("#result").show();
-// 		event.preventDefault();
-// 	});
-// });
+$(document).ready(function() {
+	$("form#sides").submit(function(event) {
+		var side1 = parseInt($("input#side1").val());
+		var side2 = parseInt($("input#side2").val());
+		var side3 = parseInt($("input#side3").val());
+		var result = triangles(side1, side2, side3);
+
+		$(".triangle").text(result);
+
+		$("#result").show();
+		event.preventDefault();
+	});
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,18 @@
 describe('triangles', function() {
 	it('is an equilateral triangle if all sides are equal to each other', function() {
-		expect(triangles(2, 2, 2)).to.equal("Equilateral");
+		expect(triangles(2, 2, 2)).to.equal(" an Equilateral");
+	});
+	it('is an isosceles triangle if two sides are equal to each other', function() {
+		expect(triangles(2, 2, 3)).to.equal(" an Isosceles");
+	});
+	it('is a scalene triangle if no sides are equal to each other', function() {
+		expect(triangles(2, 3, 4)).to.equal(" a Scalene");
+	});
+	it('is not a triangle if one side is at least the length of the other two combined, scalene-like', function() {
+		expect(triangles(2, 3, 5)).to.equal(" NOT a");
+	});
+	it('is not a triangle if one side is at least the length of the other two combined, isosceles-like', function() {
+		expect(triangles(3, 3, 6)).to.equal(" NOT a");
 	});
 
 });
